42a9b101d1b697b8a11819e37e863f80c012d9d0
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-20 12:49:14 -0600

    Update

--- Index.js
+++ Index.js
@@ -1,4 +1,16 @@
 console.log('Hello World');
 
-var name="Martin"
+// create 10 variables about yourself
+let Firstname="Martin"
+let Age="16"
+let Hair colour="Black"
+let Eye colour= "Dark Brown"
+let Nationality= "Chinese/Canadian"
+let Personality= "Likes academics and games and anime/kpop"
+let Weight= "129lbs"
+let Height= "171cm"
+let Age= "16"
+let Born at= "Canada, Ontario, Toronto"
+let Social skills= "Intermediate"
+//print variabled to console
 console.log(name);
