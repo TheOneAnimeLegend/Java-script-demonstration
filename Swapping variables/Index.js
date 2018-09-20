6c37b115233c184fdc4ebff5fe1d41aab5561e7d
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-20 13:10:49 -0600

    update
    update

---
+++ Swapping variables/Index.js
@@ -0,0 +1,34 @@
+console.log('Hello World');
+
+// create 10 variables about yourself
+let Firstname = "Martin";
+let Lastname = "Yu";
+let Age ="16";
+const ageAfraid = 16;
+let Hair colour = "Black";
+let Eye colour = "Dark Brown";
+let Nationality = "Chinese/Canadian";
+let Personality = "Likes academics and games and anime/kpop";
+let Weight = "129lbs";
+let Height = "171cm";
+let Age = "16";
+let Born at = "Canada, Ontario, Toronto";
+let Social skills = "Intermediate";
+let hair = null;
+let male = true;
+//print variabled to console
+console.log(Firstname);
+console. log(Lastname);
+console. log(Age);
+console. log(ageafraid);
+console. log(Hair colour);
+console. log(Eye colour);
+console. log(Nationality);
+console. log(Personality);
+console. log(Weight);
+console. log(Height);
+console. log(Age);
+console. log(Born at);
+console. log(Social skills);
+console. log(hair)
+console. log(male)
