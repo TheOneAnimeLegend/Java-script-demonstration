f34eece63a3f93af01d5de3d8b22a16e648fdc90
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-21 13:14:57 -0600

    Update

--- Swapping variables/Index.js
+++ Swapping variables/Index.js
@@ -2,9 +2,9 @@ let Firstname = "Martin";
 let Lastname = "Yu";
 
 //Add a third variable
-Firstname = LastName
-let memory = FirstName
-Lastname = memory
+let memory = FirstName;
+Firstname = LastName;
+Lastname = memory;
 
 
 console.log("Mi primero nombre es" + LastName);
