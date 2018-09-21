6cd5c7eda80b025703ff9520d7f154bcb1ffe18b
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-21 13:12:52 -0600

    Update

--- Swapping variables/Index.js
+++ Swapping variables/Index.js
@@ -1,34 +1,11 @@
-console.log('Hello World');
-
-// create 10 variables about yourself
 let Firstname = "Martin";
 let Lastname = "Yu";
-let Age ="16";
-const ageAfraid = 16;
-let Hair colour = "Black";
-let Eye colour = "Dark Brown";
-let Nationality = "Chinese/Canadian";
-let Personality = "Likes academics and games and anime/kpop";
-let Weight = "129lbs";
-let Height = "171cm";
-let Age = "16";
-let Born at = "Canada, Ontario, Toronto";
-let Social skills = "Intermediate";
-let hair = null;
-let male = true;
-//print variabled to console
-console.log(Firstname);
-console. log(Lastname);
-console. log(Age);
-console. log(ageafraid);
-console. log(Hair colour);
-console. log(Eye colour);
-console. log(Nationality);
-console. log(Personality);
-console. log(Weight);
-console. log(Height);
-console. log(Age);
-console. log(Born at);
-console. log(Social skills);
-console. log(hair)
-console. log(male)
+
+//Add a third variable
+Firstname = LastName
+let memory = FirstName
+Lastname = memory
+
+
+console.log("Mi primero nombre es" + LastName);
+console. log("Mi Segundo nombre es" + FirstName);
