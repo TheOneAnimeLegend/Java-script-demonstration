c4af51827f07563a5c0482a4dc5f57e2e7aaa177
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-21 13:38:36 -0600

    Update

---
+++ Swapping Variables Object start/Index.js
@@ -0,0 +1,24 @@
+console.log('Hello World');
+
+
+let MartinYu = {
+
+FirstName: "Martin";
+LastName: "Yu";
+Age: "16";
+ageAfraid: "16"
+Haircolour: "Black";
+Eyecolour: "Dark Brown";
+Nationality: "Chinese/Canadian";
+Personality: "Likes academics and games and anime/kpop";
+Weight: "129 lbs";
+Height: "171cm";
+Born: "Canada, Ontario, Toronto";
+Socialskills: "Intermediate";
+hair: Lot;
+male: true;
+}
+
+
+console.log(MartinYu);
+console.log()
