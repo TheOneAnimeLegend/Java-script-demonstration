4497e93cc142ff5481e444f7b108f2c1f3332a87
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-24 13:05:53 -0600

    Update

--- Variables array start/Index.js
+++ Variables array start/Index.js
@@ -1,27 +1,17 @@
-console.log('Hello World');
+let MartinYu = [];
 
 
-let MartinYu = {
-FirstName: "Martin",
-LastName: "Yu",
-Age: "16",
-ageAfraid: "16",
-Haircolour: "Black",
-Eyecolour: "Dark Brown",
-Nationality: "Chinese/Canadian",
-Personality: "Likes academics and games and anime/kpop",
-Weight: "129 lbs",
-Height: "171cm",
-Born: "Canada, Ontario, Toronto",
-Socialskills: "Intermediate",
-hair: true,
-male: true,
-}
+// Firstname, LastName, Age, ageAfraid, Haircolour, Eyecolour, Nationality, Personality, Weight, Height, Born, Socialskills, hair, male
+MartinYu = ["Martin", "Yu", 13, 29, null, false];
+MeganSpence = ["Megan", "Spence", 14, null, true, false, true];
+
+
+
 
 let PhoneBoi = {
   FirstName: "Phone",
   LastName: "Boi",
-}
+
 
 let memory = MartinYu.FirstName;
 MartinYu.FirstName = PhoneBoi.FirstName;
