f49948eab0d3b881eec33b8821f1162b3a053ccd
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-24 13:30:43 -0600

    Update

--- Swapping variables function/Index.js
+++ Swapping variables function/Index.js
@@ -1,16 +1,13 @@
-// Your functio goes here
-
-
 let MartinYu = [];
-let MeganSpence = [];
+
 
 // Firstname, LastName, Age, ageAfraid, Haircolour, Eyecolour, Nationality, Personality, Weight, Height, Born, Socialskills, hair, male
-MarkMercer = ["Mark", "Mercer", 13, 29, null, false];
+MartinYu = ["Martin", "Yu", 13, 29, null, false];
 MeganSpence = ["Megan", "Spence", 14, null, true, false, true];
 
-//Your code goes here
 
-// Print first names only
+
+
 let PhoneBoi = {
   FirstName: "Phone",
   LastName: "Boi",
@@ -23,6 +20,3 @@ PhoneBoi.FirstName = memory:
 
 console.log("Martin's name is " + MartinYu.FirstName);
 console.log("phone's name is " + PhoneBoi.FirstName);
-
-
-console.console.log (martinYu[1])
