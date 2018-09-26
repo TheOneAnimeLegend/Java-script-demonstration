c5528abe65d04bd9ccb68586909b80ffacc02598
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-26 13:43:49 -0600

    update

---
+++ operators/Index.js
@@ -0,0 +1,13 @@
+function simpleOperators (numberA, numberB){
+ let adding = number  + number;
+ let subtract = number - number;
+ let multiply = number * number;
+ let divide = number / number;
+ console. log (adding);
+ console. log (subtract);
+ console. log (multiply);
+ console. log (divide);
+}
+
+let number1 = [4, 7];
+simpleOperators (umber[0], number[1]);
---
+++ operators/README.md
@@ -0,0 +1,6 @@
+# operators, progress. and Notes
+introduction to operators and computational thinking
+
+
+
+## Ideas to include
