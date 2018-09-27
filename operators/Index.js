ce2f7b02bbfd74fc235c9f789d361ca949eb2a7c
TheOneAnimeLegend <43045937+TheOneAnimeLegend@users.noreply.github.com>
2018-09-27 13:22:42 -0600

    Update

--- operators/Index.js
+++ operators/Index.js
@@ -1,22 +1,23 @@
 function simpleOperators (numberA, numberB){
- let adding = numberA  + numberB;
- let subtract = numberA - numberB;
- let multiply = numberA * numberB;
- let divide = numberA / numberB;
- console. log (adding);
- console. log (subtract);
- console. log (multiply);
- console. log (divide);
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB;
+  let mutliply = numberA * numberB;
+  let divide = numberA / numberB;
+  console.log(adding);
+  console.log(subtract);
+  console.log(mutliply);
+  console.log(divide);
 }
-//skip count by 3 for 5 times starting at 5
-function skipcounting (skipcount, times, startingnumber){
- let number = startingnumber, skipCount;
- for (let i=1; i<=5; i++){
-   number += skipCount;
-   console.log(number);
+//Skipcount by ___ for ___ times starting at ___
+function skipCounting (skipCount, times, startingNumber){
+  let number = startingNumber - skipCount;
+  for (let i=1; i<=times; i++){
+    number += skipCount;
+    console.log(number);
   }
 }
-let number = [4, 7, 20, 35];
+let number = [4, 7, 13, 99];
 simpleOperators (number[0], number[1]);
 simpleOperators (number[2], number[3]);
-skipcounting (3, 6, 113);
+skipCounting (3, 6, 113);
+skipCounting (8, 11, -351);
